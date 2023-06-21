9facde514548b4837fe9bceee42d6c8357266a65
Pamela UMWALI <p.umwali@alustudent.com>
2023-06-21 20:17:43 +0200

    1-pack_web_static.py

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+from fabric.api import local
+from datetime import datetime
+
+def do_pack():
+    """Generate a .tgz archive from the contents of the web_static folder."""
+    now = datetime.now().strftime('%Y%m%d%H%M%S')
+    archive_name = 'web_static_' + now + '.tgz'
+    archive_path = 'versions/' + archive_name
+
+    # Create the versions folder if it doesn't exist
+    local('mkdir -p versions')
+
+    # Compress the web_static folder into a .tgz archive
+    result = local('tar -czvf {} web_static'.format(archive_path))
+
+    if result.failed:
+        return None
+
+    return archive_path
